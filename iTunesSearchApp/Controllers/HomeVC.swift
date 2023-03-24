35c76dbfcd1de457ebbfffa5aed92d434d612c3a
ezgiustunel <ceng.ezgiustunel@gmail.com>
2023-03-24 21:56:42 +0300

    Create project & Add gitignore

---
+++ iTunesSearchApp/Controllers/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  iTunesSearchApp
+//
+//  Created by Ezgi Üstünel on 24.03.2023.
+//
+
+import UIKit
+
+final class HomeVC: UIViewController {
+
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
