bc87737f0cf89e71d582b385550bad75bcb754ec
ezgiustunel <ceng.ezgiustunel@gmail.com>
2023-03-25 23:11:50 +0300

    Add Unit test

---
+++ iTunesSearchAppTests/MockSearchService.swift
@@ -0,0 +1,30 @@
+//
+//  MockSearchService.swift
+//  iTunesSearchAppTests
+//
+//  Created by Ezgi Üstünel on 25.03.2023.
+//
+
+import Foundation
+
+@testable import iTunesSearchApp
+
+class MockSearchService: SearchServiceDelegate {
+    var result: Result<SearchResponseModel, Error>!
+    
+    func getItems(term: String, completion: @escaping (Result<SearchResponseModel, Error>) -> Void) {
+        completion(result)
+    }
+    
+    func searchItems() -> SearchResponseModel? {
+        do {
+            guard let fileURL = Bundle.main.url(forResource: "SearchItems", withExtension: "json") else {
+                return nil
+            }
+            let data = try Data(contentsOf: fileURL)
+            return try JSONDecoder().decode(SearchResponseModel.self, from: data)
+        } catch _ {
+            return nil
+        }
+    }
+}
