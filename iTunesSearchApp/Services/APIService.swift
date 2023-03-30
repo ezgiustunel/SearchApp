d6c4ef027a0896d50269d8ed09fc3bf31ecbfd57
ezgiustunel <ceng.ezgiustunel@gmail.com>
2023-03-30 05:59:52 +0300

    Update APIService

--- iTunesSearchApp/Services/APIService.swift
+++ iTunesSearchApp/Services/APIService.swift
@@ -7,48 +7,26 @@
 
 import Foundation
 
-protocol SearchAPIProtocol {
-    func performRequest(completion:@escaping (Result<SearchResponseModel, Error>) -> Void)
-    func parseJSON(data: Data, completion:@escaping (Result<SearchResponseModel, Error>) -> Void)
-}
-
-class APIService: SearchAPIProtocol {
-    let request: URLRequest
-    lazy var configuration: URLSessionConfiguration = URLSessionConfiguration.default
-    lazy var session: URLSession = URLSession(configuration: self.configuration)
+struct APIService {
     
-    init(request: URLRequest) {
-        self.request = request
-    }
-    
-    func performRequest(completion:@escaping (Result<SearchResponseModel, Error>) -> Void) {
-        let dataTask = session.dataTask(with: self.request) { (data, response, error) in
+    static func performRequest<T>(route: APIRouter, decoder: JSONDecoder = JSONDecoder(), completion:@escaping (Result<T, Error>) -> Void) where T: Codable {
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        
+        let dataTask = session.dataTask(with: route.request) { (data, response, error) in
             if error != nil {
                 completion(.failure(CustomError.errorWithMessage(message: "error")))
             }
             if let safeData = data {
-                self.parseJSON(data: safeData) { result in
-                    switch result {
-                    case .success(let response):
-                        completion(.success(response))
-                    case .failure(let error):
-                        completion(.failure(error))
-                    }
+                guard let responseModel = try? decoder.decode(T.self, from: safeData) else {
+                    completion(Result.failure(CustomError.errorWithMessage(message: "invalid")))
+                    return
                 }
+                completion(.success(responseModel))
+            } else {
+                completion(Result.failure(CustomError.errorWithMessage(message: "invalid")))
             }
         }
         dataTask.resume()
-    }
-    
-    func parseJSON(data: Data, completion:@escaping (Result<SearchResponseModel, Error>) -> Void) {
-        let decoder = JSONDecoder()
-        do {
-            var items = SearchResponseModel()
-            items = try decoder.decode(SearchResponseModel.self, from: data)
-            completion(.success(items))
-        } catch {
-            completion(.failure(error))
-            print(error)
-        }
     }
 }
--- iTunesSearchApp/Services/SearchService.swift
+++ iTunesSearchApp/Services/SearchService.swift
@@ -13,14 +13,16 @@ protocol SearchServiceProtocol {
 
 final class SearchService: SearchServiceProtocol {
     func getItems(term: String, completion: @escaping (Result<SearchResponseModel, Error>) -> Void) {
-        let searchEndpoint = APIRouter.search(term: term)
-        let searchUrlRequest = searchEndpoint.request
-        let api = APIService(request: searchUrlRequest)
         
-        api.performRequest { result in
+        APIService.performRequest(route: APIRouter.search(term: term)) { (result:(Result<SearchResponseModel?, Error>)) in
             switch result {
             case .success(let response):
-                completion(.success(response))
+                if let response = response {
+                    completion(.success(response))
+                } else {
+                    completion(.success(SearchResponseModel()))
+                }
+                
             case .failure(let error):
                 completion(.failure(error))
             }
